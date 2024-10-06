efd6074e7de3390275a39adb3b5ba9cccd2f1b51
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-10-06 13:44:15 +0600

    appbar done

---
+++ lib/Views/bmi_calculator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BmiCalculator extends StatefulWidget {
+  const BmiCalculator({super.key});
+
+  @override
+  State<BmiCalculator> createState() => _BmiCalculatorState();
+}
+
+class _BmiCalculatorState extends State<BmiCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BMI Calculator"),
+      ),
+    );
+  }
+}
